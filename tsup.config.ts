ff645019009e563010000eba86d74cdb6a1a7e6c
thadeu <tadeuu@gmail.com>
2024-07-22 19:49:57 +0000

    set minify: true

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   target: 'es2020',
-  format: ['cjs', 'esm'],
+  format: ['cjs'],
   splitting: false,
   sourcemap: true,
   clean: true,
+  minify: true,
   dts: true
 })
