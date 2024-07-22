d904fdc78d9e18feef25fe6f5e4ee826ffa05ae3
thadeu <tadeuu@gmail.com>
2024-07-22 19:50:06 +0000

    moment load only ameria tz

--- src/timezone.ts
+++ src/timezone.ts
@@ -1,5 +1,15 @@
 import moment, { Moment } from 'moment-timezone'
 
+moment.tz.load({
+  version : 'latest',
+  zones : [
+    "America/Sao_Paulo|LMT -03 -02|36.s 30 20|01212121212121212121212121212121212121212121212121212121212121212121212121212121212121212121|-2glwR.w HdKR.w 1cc0 1e10 1bX0 Ezd0 So0 1vA0 Mn0 1BB0 ML0 1BB0 zX0 pTd0 PX0 2ep0 nz0 1C10 zX0 1C10 LX0 1C10 Mn0 H210 Rb0 1tB0 IL0 1Fd0 FX0 1EN0 FX0 1HB0 Lz0 1EN0 Lz0 1C10 IL0 1HB0 Db0 1HB0 On0 1zd0 On0 1zd0 Lz0 1zd0 Rb0 1wN0 Wn0 1tB0 Rb0 1tB0 WL0 1tB0 Rb0 1zd0 On0 1HB0 FX0 1C10 Lz0 1Ip0 HX0 1zd0 On0 1HB0 IL0 1wp0 On0 1C10 Lz0 1C10 On0 1zd0 On0 1zd0 Rb0 1zd0 Lz0 1C10 Lz0 1C10 On0 1zd0 On0 1zd0 On0 1zd0 On0 1HB0 FX0|20e6"
+  ],
+  links : [
+      'America/Sao_Paulo'
+  ]
+})
+
 const MOMENT_FORMATS = [
   'DD/MM/YYYY',
   'YYYY/MM/DD',
